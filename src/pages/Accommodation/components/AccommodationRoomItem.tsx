c9371e5ca964ab1f2781be373ad5d0cea320ca6c
JinJeongMin <jdsjm2@naver.com>
2023-11-23 00:20:23 +0900

    design: AccommodationRoomItem 컴포넌트 디자인

--- src/pages/Accommodation/components/AccommodationRoomItem.tsx
+++ src/pages/Accommodation/components/AccommodationRoomItem.tsx
@@ -1,19 +1,107 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import { StarFilled, ShoppingCartOutlined } from '@ant-design/icons';
+import { Heading, Text, Button } from '@chakra-ui/react';
 import { theme } from '../../../styles/theme';
 
 function AccommodationRoomItem() {
+  const [cartHover, setCartHover] = useState(false);
+
+  const handleCartMouseEnter = () => {
+    setCartHover(true);
+  };
+
+  const handleCartMouseLeave = () => {
+    setCartHover(false);
+  };
+
   return (
-    <AccommodationRoomItemWrapper>
-      AccommodationItem
-    </AccommodationRoomItemWrapper>
+    <StyledAccommodationRoomItemWrapper>
+      <StyledAccommodationRoomImg />
+      <StyledAccommodationRoomTitle>
+        <StyledAccommodationRoomTitleBox style={{ marginBottom: '0.5rem' }}>
+          <Heading as="h4" size="sm">
+            101호
+          </Heading>
+          <div>
+            <StarFilled
+              style={{ color: `${theme.colors.blue400}`, fontSize: '0.8rem' }}
+            />
+            <StyledStarDigit>4.90</StyledStarDigit>
+          </div>
+        </StyledAccommodationRoomTitleBox>
+        <StyledAccommodationRoomTitleBox>
+          <div>
+            <Text as="p" size="sm" color="gray.84">
+              최소 2명 / 최대 4명
+            </Text>
+            <Text as="p" size="sm">
+              ￦83,400원/박
+            </Text>
+          </div>
+          <StyledAccommodationRoomTitleBoxItem>
+            {/* 툴팁 작업 필요 */}
+            <ShoppingCartOutlined
+              style={{
+                fontSize: '30px',
+                color: cartHover
+                  ? `${theme.colors.basic}`
+                  : `${theme.colors.gray300}`,
+                marginRight: '1rem',
+                cursor: 'pointer',
+              }}
+              onMouseEnter={handleCartMouseEnter}
+              onMouseLeave={handleCartMouseLeave}
+            />
+            <Button
+              variant="blue"
+              size="lg"
+              style={{ width: '100px', height: '40px' }}
+            >
+              예약하기
+            </Button>
+          </StyledAccommodationRoomTitleBoxItem>
+        </StyledAccommodationRoomTitleBox>
+      </StyledAccommodationRoomTitle>
+    </StyledAccommodationRoomItemWrapper>
   );
 }
 
 export default AccommodationRoomItem;
 
-const AccommodationRoomItemWrapper = styled.div`
+const StyledAccommodationRoomItemWrapper = styled.div`
   width: 100%;
-  height: 380px;
+
   margin-bottom: 2rem;
+`;
+
+const StyledAccommodationRoomImg = styled.div`
+  width: 100%;
+  height: 300px;
+  border-radius: 15px;
   background-color: ${theme.colors.gray100};
+`;
+
+const StyledAccommodationRoomTitle = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 1rem;
+`;
+
+const StyledAccommodationRoomTitleBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledStarDigit = styled.span`
+  margin-left: 0.5rem;
+  font-size: 0.8rem;
+`;
+
+const StyledAccommodationRoomTitleBoxItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
