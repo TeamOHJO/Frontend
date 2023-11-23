4c2f7da9a5281c1f2f27f28bb6bbf56ecc177ba6
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-24 01:55:38 +0900

    feat: 리뷰 페이지 레이아웃 제작

---
+++ src/pages/customerReview/index.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import ReviewPageTitle from './ReviewPageTitle';
+import ReviewSubtitle from './ReviewSubtitle';
+import ReviewCard from '../../components/ReviewCard';
+
+const CustomerReview = () => {
+  return (
+    <StyledLayout>
+      <ReviewPageTitle />
+      <ReviewSubtitle />
+      <ReviewCard />
+    </StyledLayout>
+  );
+};
+
+export default CustomerReview;
+
+const StyledLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  /* min-height: 100vh; */
+  min-height: calc(var(--var, 1vh) * 100);
+`;
