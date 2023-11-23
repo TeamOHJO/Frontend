200b609fcdb27cd9bc4ce72a1f321c5c035f139a
JinJeongMin <jdsjm2@naver.com>
2023-11-23 16:24:44 +0900

    design: RoomMainImg 컴포넌트 디자인

--- src/pages/Room/RoomMainImg.tsx
+++ src/pages/Room/RoomMainImg.tsx
@@ -1,8 +1,19 @@
 import styled from '@emotion/styled';
 import { theme } from '../../styles/theme';
+import SwiperComponent from '../../components/Swiper/SwiperComponent';
 
 function RoomMainImg() {
-  return <StyledRoomMainImgWrapper>ImageCarousel</StyledRoomMainImgWrapper>;
+  const images: string[] = [
+    'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
+  ];
+  return (
+    <StyledRoomMainImgWrapper>
+      <SwiperComponent borderRadius="0" images={images} />
+    </StyledRoomMainImgWrapper>
+  );
 }
 
 export default RoomMainImg;
