0451ee7c6e888363dede88529fa8dff00284ace8
zoeyourlife <201814112@office.skhu.ac.kr>
2023-11-24 01:55:10 +0900

    feat: 리뷰 페이지 타이틀 및 서브 타이틀 컴포넌트 제작

---
+++ src/pages/customerReview/ReviewSubtitle.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+const ReviewSubtitle = () => {
+  return (
+    <Text fontSize="1rem" mt="2rem" ml="2rem" fontWeight="bold">
+      리뷰 내역
+    </Text>
+  );
+};
+
+export default ReviewSubtitle;
