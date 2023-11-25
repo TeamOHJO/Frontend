b2482709cd11206369c4e2ec454bc2a47bb1b641
gkdus2217 <gkdus2217@gmail.com>
2023-11-26 00:54:29 +0900

    feat : 로그인 api 연결

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { JoinData, LoginData } from './type';
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  headers: {
+    'content-type': import.meta.env.VITE_CONTENT_TYPE,
+  },
+});
+
+export const postLogin = async (loginData: LoginData) => {
+  const res = await client.post('/login', loginData);
+  return res.data;
+};
+
+export const postJoin = async (joinData: JoinData) => {
+  const res = await client.post('/user/signup', joinData);
+  return res.data;
+};
