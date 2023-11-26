cb1665673bbd4735997bf7f455cae84106600208
TeukHee <rlaxmrgml@naver.com>
2023-11-26 18:04:44 +0900

    test: 목데이터추가 & 연동

--- src/pages/Home/ContentsContainer.tsx
+++ src/pages/Home/ContentsContainer.tsx
@@ -1,110 +1,28 @@
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import HomeCard from './HomeCard';
 import { HomeCardProps } from '../../@types/interface';
 
-const dummy = [
-  {
-    name: '신라호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.9,
-    price: 123400,
-  },
-  {
-    name: '백제호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.8,
-    price: 122000,
-  },
-  {
-    name: '미국호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.5,
-    price: 132000,
-  },
-  {
-    name: '일본호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 3.0,
-    price: 52000,
-  },
-  {
-    name: '중국호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.2,
-    price: 122000,
-  },
-  {
-    name: '뉴욕호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.1,
-    price: 124300,
-  },
-  {
-    name: '파리호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.5,
-    price: 124200,
-  },
-  {
-    name: '로마호텔',
-    images: [
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/Q7pR7uazGgU/maxresdefault.jpg',
-    ],
-    category: '호텔/리조트',
-    score: 4.9,
-    price: 124300,
-  },
-];
-
 const ContentsContainer = () => {
+  const [list, setList] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      'http://localhost:5173/data/accommodationList.json',
+      {
+        method: 'GET',
+      },
+    );
+    setList(await response.json());
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <StyledContainer>
-      {dummy.map((e: HomeCardProps) => {
+      {list.map((e: HomeCardProps) => {
         return (
           <HomeCard
             key={e.price * e.score}
