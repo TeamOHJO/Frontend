0d682b34fc0dbbaa7c077029e565daa3c6f4c4a7
TeukHee <rlaxmrgml@naver.com>
2023-11-27 14:21:52 +0900

    feat: 검색필터링 상태관리

--- src/states/atom.ts
+++ src/states/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 import { AlertData } from '../@types/interface';
+import { getTomorrow } from '../utils/utils';
 
 export const toastPopupState = atom<AlertData>({
   key: 'toastPopupState',
@@ -8,5 +9,21 @@ export const toastPopupState = atom<AlertData>({
 
 export const loginTabState = atom<number>({
   key: 'loginTabState',
+  default: 0,
+});
+
+export const searchFilteredState = atom({
+  key: 'searchFilteredState',
+  default: {
+    category: 'HOTEL',
+    isDomestic: true,
+    startTime: new Date(),
+    endtime: getTomorrow(),
+    numberOfPerson: 2,
+  },
+});
+
+export const searchPages = atom({
+  key: 'searchPage',
   default: 0,
 });
