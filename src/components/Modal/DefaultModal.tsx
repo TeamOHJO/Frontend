89ba14bf4704ae52632c6d588163a4ab2f164beb
JinJeongMin <jdsjm2@naver.com>
2023-11-27 14:08:56 +0900

    fix: default 모달 수정

--- src/components/Modal/DefaultModal.tsx
+++ src/components/Modal/DefaultModal.tsx
@@ -25,25 +25,31 @@ const DefaultModal: FunctionComponent<ModalProps> = ({
     motionPreset="slideInBottom"
   >
     <ModalOverlay background="none" />
-    <ModalContent width="auto" border="1px solid" borderColor="#B8B8B8">
+    <ModalContent width="280px" border="1px solid" borderColor="#B8B8B8">
       <ModalHeader fontSize="lg">{modalData.heading}</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
         <Text size="md">{modalData.text}</Text>
       </ModalBody>
-
-      <ModalFooter gap="10px">
-        <Button variant="grayFull" onClick={onClose} width="auto">
+      <ModalFooter gap="10px" width="100%">
+        <Button
+          variant="grayFull"
+          width="auto"
+          size="md"
+          minWidth="0"
+          onClick={onClose}
+        >
           아니오
         </Button>
         <Button
           colorScheme="blue"
           size="md"
+          width="auto"
+          minWidth="0"
           onClick={() => {
             modalFunc();
             onClose();
           }}
-          width="auto"
         >
           예
         </Button>
