e66697eca3be7ff472a1e0f559d6bd39d0dad6b8
JinJeongMin <jdsjm2@naver.com>
2023-11-28 01:15:47 +0900

    design: 좋아요 버튼 애니메이션 추가

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
 import { useState } from 'react';
-import { HeartTwoTone, HeartFilled } from '@ant-design/icons';
+import { HeartFilled, HeartOutlined } from '@ant-design/icons';
 import { theme } from '../styles/theme';
 
 interface HeartProps {
@@ -15,19 +16,39 @@ function Heart({ isLiked, size }: HeartProps) {
     setIsHeart(!isHeart);
   }
 
+  const Bounce = keyframes`
+  0%{
+    transform:scale(1)
+  }
+  20%{
+    transform:scale(0.9)
+  }
+  40%{
+    transform:scale(1.1)
+  }
+  100%{
+    transform:scale(1)
+  }
+`;
+
+  const StyledHeartFilled = styled(HeartFilled)`
+    font-size: ${size};
+    color: ${theme.colors.red500};
+    animation: 300ms ${Bounce} forwards;
+  `;
+
+  const StyledHeartOutlined = styled(HeartOutlined)`
+    font-size: ${size};
+    color: ${theme.colors.red500};
+    animation: 300ms ${Bounce} forwards;
+  `;
+
   return (
     <StyledHeart>
       {isHeart ? (
-        <HeartFilled
-          onClick={() => handleIsHeart()}
-          style={{ fontSize: size, color: theme.colors.red500 }}
-        />
+        <StyledHeartFilled onClick={() => handleIsHeart()} />
       ) : (
-        <HeartTwoTone
-          onClick={() => handleIsHeart()}
-          twoToneColor={theme.colors.red500}
-          style={{ fontSize: size }}
-        />
+        <StyledHeartOutlined onClick={() => handleIsHeart()} />
       )}
     </StyledHeart>
   );
