ffb397af2f155116eea8b72db1754efdc5558bb3
JinJeongMin <jdsjm2@naver.com>
2023-11-30 03:29:39 +0900

    feat: Id 없는 경우 쫓아내기

--- src/pages/Accommodation/index.tsx
+++ src/pages/Accommodation/index.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import AccommodationNavi from './AccommodationNavi';
 import AccommodationMainImages from './AccommodationMainImg';
@@ -30,6 +30,7 @@ function Accommodation() {
   const [accommodationSelectVisitors] = useRecoilState<number>(accommodationSelectVisitorsState);
 
   const accessToken = getCookie('token');
+  const navigate = useNavigate();
 
   const fetchData = async () => {
     if (accessToken) {
@@ -46,8 +47,11 @@ function Accommodation() {
             Authorization: `Bearer ${accessToken}`,
           },
         },
-      );
-      setAccommodationDetailData(await response.json());
+      ).then((res: any) => {
+        if (!res.ok) navigate('/');
+        return res.json();
+      });
+      setAccommodationDetailData(await response);
     } else {
       const response = await fetch(
         `https://yanoljaschool.site:8080/accommodation/detail/${
@@ -58,8 +62,11 @@ function Accommodation() {
         {
           method: 'GET',
         },
-      );
-      setAccommodationDetailData(await response.json());
+      ).then((res: any) => {
+        if (!res.ok) navigate('/');
+        return res.json();
+      });
+      setAccommodationDetailData(await response);
     }
   };
 
