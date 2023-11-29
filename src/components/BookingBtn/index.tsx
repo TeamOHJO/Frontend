ae2f8b9108370b0c23204fecbe966233a888e4a1
zoeyourlife <jaehun990909@gmail.com>
2023-11-29 18:30:11 +0900

    fix: netlify 배포 오류 해결

--- src/components/BookingBtn/index.tsx
+++ src/components/BookingBtn/index.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
 import { Button } from '@chakra-ui/react';
 import ReservationModal from '../Modal/ReservationModal';
-import { ReservationInfo } from '../../@types/interface';
-import { postReservation } from '../../api';
 
-function BookingBtn(roomsId: number) {
+function BookingBtn() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isPaymentCompleted, setIsPaymentCompleted] = useState(false);
-  const [reservationInfo, setReservationInfo] =
-    useState<ReservationInfo | null>(null);
 
   const handleButtonClick = () => {
     setIsModalOpen(true);
@@ -19,17 +15,7 @@ function BookingBtn(roomsId: number) {
     setIsModalOpen(false);
   };
 
-  const handleConfirm = async () => {
-    try {
-      // 예약 정보를 서버에 보내는 로직
-      if (reservationInfo) {
-        await postReservation(roomsId, reservationInfo);
-        setIsPaymentCompleted(true);
-      }
-    } catch (error) {
-      // 예약 실패 시의 처리
-      console.error('Error during reservation:', error);
-    }
+  const handleConfirm = () => {
     handleCloseModal();
   };
   return (
@@ -45,7 +31,6 @@ function BookingBtn(roomsId: number) {
         isOpen={isModalOpen}
         onClose={handleCloseModal}
         onConfirm={handleConfirm}
-        // setReservationInfo={setReservationInfo}
       />
     </>
   );
