8d2b66f9057135cf6353947b00e0e4ffec431dbb
TeukHee <rlaxmrgml@naver.com>
2023-12-04 16:20:27 +0900

    feat: 로그인버튼 쓰로틀링 적용

--- src/pages/Login/LoginTabContent.tsx
+++ src/pages/Login/LoginTabContent.tsx
@@ -10,8 +10,10 @@ import { setCookies, validateField } from '../../utils/utils';
 import { postLogin } from '../../api';
 import { ErrorData } from '../../api/type';
 import { toastPopupState } from '../../states/atom';
+import UseThrottle from '../../hooks/useThrottle';
 
 const LoginTabContent = () => {
+  const handleThrottle = UseThrottle();
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -29,25 +31,28 @@ const LoginTabContent = () => {
 
   const handleLoginSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    try {
-      const res = await postLogin(formData);
-      const { data } = res;
-      await setCookies(data.email, data.name, data.token);
-      navigate('/');
-    } catch (error) {
-      if (isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        const data = axiosError.response?.data as ErrorData;
-        if (data.code === 400) {
-          setShowAlert({
-            active: true,
-            message: '이메일 또는 비밀번호가 다릅니다.',
-          });
+    handleThrottle(async () => {
+      try {
+        console.log('로그인');
+        const res = await postLogin(formData);
+        const { data } = res;
+        await setCookies(data.email, data.name, data.token);
+        navigate('/');
+      } catch (error) {
+        if (isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+          const data = axiosError.response?.data as ErrorData;
+          if (data.code === 400) {
+            setShowAlert({
+              active: true,
+              message: '이메일 또는 비밀번호가 다릅니다.',
+            });
+          }
+        } else {
+          console.log('Axios 에러가 아닌 다른 에러가 발생했습니다:', error);
         }
-      } else {
-        console.log('Axios 에러가 아닌 다른 에러가 발생했습니다:', error);
       }
-    }
+    });
   };
 
   // 에러가 Axios 에러인지 확인하는 함수
